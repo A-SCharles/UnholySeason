83d8f60acaf06c13654fa2c848cfc96c188999fa
Abdus-Samad Charles <a.charles.edu@gmail.com>
2022-07-09 09:46:55 +0200

    updated

--- src/views/animes.vue
+++ src/views/animes.vue
@@ -1,13 +1,54 @@
 <template>
   <h2>I am Working</h2>
+  <div v-if="animes">
+    <div class="container">
+      <div class="row">
+        <!-- <lists v-for="anime of animes" :key="anime.id" /> -->
+        <div
+          class="card m-3 p-3"
+          v-for="anime of animes"
+          :key="anime.id"
+          style="width: 18rem"
+        >
+          <img :src="anime.image" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{{ anime.name }}</h5>
+            <p class="card-text"></p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div v-else>
+    <h2>Loading ...</h2>
+  </div>
 </template>
 
 <script>
+import lists from "@/components/lists.vue";
 export default {
+  components: { lists },
+  data() {
+    return {
+      animes: null,
+    };
+  },
 
-}
+  mounted() {
+    fetch("http://localhost:3000/anime")
+      .then(res => res.json())
+      .then(data => this.animes = data)
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
+.card img {
+  height: 125px;
+  width: 100%;
+}
 
 </style>
