65f83f2312c501c5897e62a2e4251e3150a6a88f
joshe <JosheTheys@gmail.com>
2022-07-11 15:59:02 +0200

    auto-commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about container" style="padding-right: 200px; padding-left: 200px;">
+    <h1>Our Unholy Story</h1>
+    <p>What's your favourite season? We like our's a little...Unholy. </p>
+    <p>The creators of unholy season wanted to create a safe space where lovers of gore could find community in each other. So scroll through your fellow users recommondations to add to your watch list and recommend some the goriest, horrifying anime you know. We want to hear what you think, afterall this is a place where even the dead tell stories... So remember to comment and interact - no-one likes a dead fish.</p>
+    <p>Welcome to our nightmare. I think you're gonna like it.</p> 
+
+    <p style="color:red">P.S. If you are reading this, then you are blissfully unaware what is creeping up behind you.</p>
   </div>
 </template>
+
