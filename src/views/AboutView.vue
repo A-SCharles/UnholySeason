9096d9052c7ecbadb2496c4199463532d12d4777
joshe <JosheTheys@gmail.com>
2022-07-12 11:58:32 +0200

    auto-commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="about container" style="padding-right: 200px; padding-left: 200px;">
-    <h1>Our Unholy Story</h1>
-    <p>What's your favourite season? We like our's a little...Unholy. </p>
-    <p>The creators of unholy season wanted to create a safe space where lovers of gore could find community in each other. So scroll through your fellow users recommondations to add to your watch list and recommend some the goriest, horrifying anime you know. We want to hear what you think, afterall this is a place where even the dead tell stories... So remember to comment and interact - no-one likes a dead fish.</p>
-    <p>Welcome to our nightmare. I think you're gonna like it.</p> 
+    <h1  style="color:red">Our Unholy Story</h1>
+    <p style="color:white">What's your favourite season? We like our's a little...Unholy. </p>
+    <p style="color:white">The creators of unholy season wanted to create a safe space where lovers of gore could find community in each other. So scroll through your fellow users recommondations to add to your watch list and recommend some the goriest, horrifying anime you know. We want to hear what you think, afterall this is a place where even the dead tell stories... So remember to comment and interact - no-one likes a dead fish.</p>
+    <p style="color:white">Welcome to our nightmare. I think you're gonna like it.</p> 
 
     <p style="color:red">P.S. If you are reading this, then you are blissfully unaware what is creeping up behind you.</p>
   </div>
+<h1  style="color:red">Our Partners in Crime</h1>
+  <div class="card-group">
+  <div class="card">
+    <img src="https://i.postimg.cc/W4JCSdLz/hdrs4hh26kd81.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Xena</h5>
+      <p class="card-text">description</p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://i.postimg.cc/Bn03LxRN/ken-kaneki.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Ridhaa</h5>
+      <p class="card-text">The silent killer that creeps behind you.</p>
+    </div>
+  </div>
+   <div class="card">
+    <img src="https://i.postimg.cc/65JWNJ68/black-girl.webp" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">JJ</h5>
+      <p class="card-text">Sugar and spice and your torn up guts are the ingredients to create a perfect girl.</p>
+    </div>
+  </div>
+   <div class="card">
+    <img src="https://i.postimg.cc/FR7qYRph/squirtle1.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Charles</h5>
+      <p class="card-text">A kind smile hides the gleam of jaggered fangs.</p>
+    </div>
+  </div>
+</div>
 </template>
 
